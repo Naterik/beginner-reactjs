183c3791236f18bf9226e6ad4ab2515a31949487
Naterik <lukhuong190703@gmail.com>
2025-06-22 19:41:30 +0700

    router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.scss';
-
+import Header from './components/Header/Header';
+import { Link } from "react-router-dom";
 const App = () => {
   return (
     <div className="app-container">
-      Hello World
-      <button className='btn btn-primary'>Test</button>
+      <Header />
+      <button><Link to="/user">Go go User</Link></button>
+      <button><Link to="/admin">Go go Admin</Link></button>
     </div>
   );
 }
